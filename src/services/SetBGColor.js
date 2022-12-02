5733c6742a51fc3dc9db3610270f1a7edba5e8c1
Boris-Maslenov <ya.maslenov@ya.ru>
2022-12-02 22:25:21 +0300

    replacing if with switch

--- src/services/SetBGColor.js
+++ src/services/SetBGColor.js
@@ -10,20 +10,35 @@ export class SetBGColor {
 
     init(number){
         this.body.style.transition = 'all 0.5s';
-        if(number < 4){
-            this.setColor('#FF0033')
-        }else if(number < 5) {
-            this.setColor('#FF3333') ;
-        }else if(number < 6) {
-            this.setColor('#FF9933');
-        }else if(number < 7) {
-            this.setColor('#FFCC00');
-        }else if(number < 8) {
-            this.setColor('#99FFCC');
-        }else if(number < 9) {
-            this.setColor('#66FF00');
-        }else if(number < 10) {
-            this.setColor('#00CC33');
+        switch(number){
+            case 3 :
+                this.setColor('#FF3427')
+                break;
+            case 4 :
+                this.setColor('#FF663D')
+            break;
+            case 5 :
+                this.setColor('#FF9641');
+            break;
+            case 6 :
+                this.setColor('#FFCA41');
+            break;
+            case 7 :
+                this.setColor('#FFFD60');
+            break;
+            case 8 :
+                this.setColor('#A1FF3C');
+            break;
+            case  9 :
+                this.setColor('#4CFF49');
+            break;
+            case  10 :
+                this.setColor('#2CDB38');
+            break;
+                default:
+                    this.setColor('#2CDB38'); 
         }
     }
+
+
 }
